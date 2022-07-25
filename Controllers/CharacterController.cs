46ab2ece3ae6d7142cb0ecea0749ff10f5403f14
Andy Million <amillion@atiba.com>
2022-07-25 15:42:23 -0400

    Added 2 Dtos and updated Controller and Services files

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using dotnet_rpg.Services.CharacterService;
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers;
@@ -14,19 +15,19 @@ public class CharacterController : ControllerBase
     }
     
     [HttpGet("GetAll")]
-    public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+    public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
     {
         return Ok(await _characterService.GetAllCharacters());
     }
     
     [HttpGet("{id}")]
-    public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+    public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
     {
         return Ok(await _characterService.GetCharacterById(id));
     }
 
     [HttpPost]
-    public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+    public async Task<ActionResult<ServiceResponse<List<AddCharacterDto>>>> AddCharacter(Character newCharacter)
     {
         return Ok(await _characterService.AddCharacter(newCharacter));
     }
