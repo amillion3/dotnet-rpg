98898bd04d74deac2690bf77ca93a2f636296ca7
Andy Million <amillion@atiba.com>
2022-07-26 16:49:31 -0400

    finished JWT auth and allowed use of Auth during Swagger testing

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace dotnet_rpg.Data;
 
 public class AuthRepository : IAuthRepository
 {
     private readonly DataContext _context;
-    public AuthRepository(DataContext context)
+    private readonly IConfiguration _configuration;
+    public AuthRepository(DataContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
     
     public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -50,7 +55,7 @@ public class AuthRepository : IAuthRepository
         }
         else
         {
-            response.Data = user.Id.ToString();
+            response.Data = CreateToken(user);
         }
 
         return response;
@@ -82,8 +87,32 @@ public class AuthRepository : IAuthRepository
         using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
         {
             var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            
             return computeHash.SequenceEqual(passwordHash);
         }
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new List<Claim> {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+            .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+        SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(1),
+            SigningCredentials = creds
+        };
+
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
