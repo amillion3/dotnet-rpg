5b562f8453d9efb7f8cb7ad6d66a4250c7c76458
Andy Million <amillion@atiba.com>
2022-07-26 13:43:54 -0400

    Added one to many relationship between Users (one) and Characters (many)

--- Models/User.cs
+++ Models/User.cs
@@ -6,4 +6,5 @@ public class User
     public string Username { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public List<Character>? Characters { get; set; }
 }
