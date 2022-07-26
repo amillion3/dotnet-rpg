4feee06f8d5ad1874b056db00c2e824f19075b40
Andy Million <amillion@atiba.com>
2022-07-26 15:02:10 -0400

    Added UserRegisterDto and finished method for Registering a new User

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthRepository _authRepo;
+    
+    public AuthController(IAuthRepository authRepo)
+    {
+        _authRepo = authRepo;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto  request)
+    {
+        var response = await _authRepo.Register(
+            new User { Username = request.Username }, request.Password
+        );
+
+        if (!response.Success)
+        {
+            if (string.IsNullOrWhiteSpace(response.Message)) response.Message = "Error during registration.";
+            return BadRequest(response);
+        }
+
+        response.Message = "Registration successful";
+        return Ok(response);
+    }
+}
