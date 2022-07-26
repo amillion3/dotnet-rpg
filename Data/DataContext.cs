8c1fc2074deacfc114db37d06a3656c409380368
Andy Million <amillion@atiba.com>
2022-07-26 12:10:26 -0400

    removed MySQL connection bits, switched back to MSSQL connection bits, updated Character CRUD methods to use EF

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,25 +4,10 @@ namespace dotnet_rpg.Data;
 
 public class DataContext : DbContext
 {
-    // public DataContext(DbContextOptions<DataContext> options) : base(options)
-    // {
-    //     
-    // }
-    //
-    // public DbSet<Character> Characters {get; set; }
-    protected readonly IConfiguration Configuration;
-
-    public DataContext(IConfiguration configuration)
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        Configuration = configuration;
+        
     }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // connect to mysql with connection string from app settings
-        var connectionString = Configuration.GetConnectionString("MySql");
-        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-    }
-
+    
     public DbSet<Character> Characters {get; set; }
 }
--- Migrations/20220726153433_InitialCreate.cs
+++ Migrations/20220726153433_InitialCreate.cs
@@ -1,25 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace dotnet_rpg.Migrations
 {
-    public partial class InitialCreateMySql : Migration
+    public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterDatabase()
-                .Annotation("MySql:CharSet", "utf8mb4");
-
             migrationBuilder.CreateTable(
                 name: "Characters",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(type: "longtext", nullable: false)
-                        .Annotation("MySql:CharSet", "utf8mb4"),
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     HitPoints = table.Column<int>(type: "int", nullable: false),
                     Strength = table.Column<int>(type: "int", nullable: false),
                     Defenses = table.Column<int>(type: "int", nullable: false),
@@ -29,8 +24,7 @@ namespace dotnet_rpg.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Characters", x => x.Id);
-                })
-                .Annotation("MySql:CharSet", "utf8mb4");
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
