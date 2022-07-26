b037ffa3e7677f5033c0cbf4d0e1daa84f7388a7
Andy Million <amillion@atiba.com>
2022-07-26 14:17:28 -0400

    Added Register method and a check to see if User exists already

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,10 +1,35 @@
-﻿namespace dotnet_rpg.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data;
 
 public class AuthRepository : IAuthRepository
 {
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    private readonly DataContext _context;
+    public AuthRepository(DataContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    
+    public async Task<ServiceResponse<int>> Register(User user, string password)
+    {
+        ServiceResponse<int> response = new ServiceResponse<int>();
+
+        if (await UserExists(user.Username))
+        {
+            response.Success = false;
+            response.Message = "User already exists.";
+            return response;
+        }
+
+        CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        response.Data = user.Id;
+        return response;
     }
 
     public Task<ServiceResponse<string>> Login(string username, string password)
@@ -12,8 +37,22 @@ public class AuthRepository : IAuthRepository
         throw new NotImplementedException();
     }
 
-    public Task<bool> UserExists(string username)
+    public async Task<bool> UserExists(string username)
     {
-        throw new NotImplementedException();
+        if (await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new System.Security.Cryptography.HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
     }
 }
